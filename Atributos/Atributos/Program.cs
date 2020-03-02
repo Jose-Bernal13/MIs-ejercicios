53115909768f841be82c5b8e11c2987bdfe1663f
Juan_Bernal <jh47819@gmail.com>
2020-03-02 11:46:24 -0500

    Guardar

---
+++ Atributos/Atributos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            aprendiz apsena = new aprendiz();
+            apsena.Nombres = "Juan Jose";
+            apsena.Apellidos = "Hernandez Bernal";
+            apsena.Direccion = " Cra 7 # 7 - 43";
+            apsena.MunicipioResidencia = "Acevedo";
+            Saludo(apsena);
+        }
+        private static void Saludo(aprendiz apsena)
+        {
+            Console.WriteLine("Bienvenido, Sr(a) " + apsena.Nombres + " " + apsena.Apellidos);
+            Console.WriteLine("Su municipio de residencia es: " + apsena.MunicipioResidencia);
+            Console.Write("Su dirección es: " + apsena.Direccion );
+            Console.ReadLine();
+
+        }
+    }
+
